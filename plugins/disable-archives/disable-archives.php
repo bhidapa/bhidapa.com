6327ff33fb17d9cc61a259a1a5204d373ba197a1
enisdenjo <badurinadenis@gmail.com>
2022-01-01 16:39:38 +0100

    copy over some custom plugins

---
+++ plugins/disable-archives/disable-archives.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Plugin Name: Disable archives
+ * Description: Disables category, tag, date and author archives by redirecting to a 404.
+ * Version: 1.0
+ * Author: BHIDAPA
+ * Author URI: https://bhidapa.ba
+ */
+
+if (!defined('ABSPATH')) {
+  exit; // Exit if accessed directly.
+}
+
+add_action('template_redirect', 'disable_wp_archives');
+
+function disable_wp_archives()
+{
+  if (is_category() || is_tag() || is_date() || is_author()) {
+    global $wp_query;
+    $wp_query->set_404();
+  }
+}
---
+++ plugins/redirect-postname-to-category/redirect-postname-to-category.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Plugin Name: Redirect postname to category
+ * Description: Adds a 404 filter redirect from `/%postname%/` to `/%category%/%postname%/`.
+ * Version: 1.0
+ * Author: BHIDAPA
+ * Author URI: https://bhidapa.ba
+ */
+
+if (!defined('ABSPATH')) {
+  exit; // Exit if accessed directly.
+}
+
+add_filter('404_template', 'redirect_postname_to_category');
+
+function redirect_postname_to_category($template)
+{
+  if (!is_404()) {
+    return $template;
+  }
+
+  global $wp_rewrite;
+  global $wp_query;
+
+  if ('/%category%/%postname%/' !== $wp_rewrite->permalink_structure) {
+    return $template;
+  }
+
+  if (!$post = get_page_by_path($wp_query->query['category_name'], OBJECT, 'post')) {
+    return $template;
+  }
+
+  $permalink = get_permalink($post->ID);
+
+  wp_redirect($permalink, 301);
+  exit;
+}
